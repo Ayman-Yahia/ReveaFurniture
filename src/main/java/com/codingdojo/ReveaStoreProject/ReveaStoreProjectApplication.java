cef68ada3e95beb5e85895b008140a53e7fd5732
Ayman Yahia <ayman.ya1997@gmail.com>
2021-07-01 19:33:30 +0300

    start project

---
+++ src/main/java/com/codingdojo/ReveaStoreProject/ReveaStoreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.ReveaStoreProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReveaStoreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReveaStoreProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingdojo/ReveaStoreProject/ReveaStoreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.ReveaStoreProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReveaStoreProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
