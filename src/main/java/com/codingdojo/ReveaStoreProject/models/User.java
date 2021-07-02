964f301cd6a9dbeb88965d532abf4c03c067c930
Ayman Yahia <ayman.ya1997@gmail.com>
2021-07-02 21:32:08 +0300

    user model update

--- src/main/java/com/codingdojo/ReveaStoreProject/models/User.java
+++ src/main/java/com/codingdojo/ReveaStoreProject/models/User.java
@@ -39,12 +39,9 @@ public class User {
 	@NotNull
 	@Email(message="Email must be valid")
 	private String email;
-<<<<<<< HEAD
 	@Size(min=8, message="Password must be greater than 8 characters")
-=======
 	private String first_name;
 	private String last_name;
->>>>>>> branch 'master' of https://github.com/Ayman-Yahia/ReveaFurniture.git
 	private String password;
     @Transient
     private String passwordConfirmation;
@@ -67,7 +64,7 @@ public class User {
     }
     
     public Long getId() {
-<<<<<<< HEAD
+
 		return id;
 	}
 
@@ -92,57 +89,9 @@ public class User {
 		this.lastName = lastName;
 	}
 
-	public String getEmail() {
-		return email;
-	}
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
 
-	public String getPassword() {
-		return password;
-	}
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getPasswordConfirmation() {
-		return passwordConfirmation;
-	}
-
-	public void setPasswordConfirmation(String passwordConfirmation) {
-		this.passwordConfirmation = passwordConfirmation;
-	}
-
-	public List<Role> getRoles() {
-		return roles;
-	}
-
-	public void setRoles(List<Role> roles) {
-		this.roles = roles;
-	}
-
-	public List<Cart> getCarts() {
-		return carts;
-	}
-
-	public void setCarts(List<Cart> carts) {
-		this.carts = carts;
-	}
-
-	public List<Order> getOrders() {
-		return orders;
-	}
-
-	public void setOrders(List<Order> orders) {
-		this.orders = orders;
-	}
-
-=======
-        return id;
-    }
     
     public String getEmail() {
 		return email;
@@ -204,7 +153,6 @@ public class User {
 	public void setOrders(List<Order> orders) {
 		this.orders = orders;
 	}
->>>>>>> branch 'master' of https://github.com/Ayman-Yahia/ReveaFurniture.git
 	@PrePersist
     protected void onCreate(){
         this.createdAt = new Date();
