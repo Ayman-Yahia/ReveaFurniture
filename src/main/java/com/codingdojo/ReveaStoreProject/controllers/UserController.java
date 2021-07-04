55bb3fce6354fa306f0751f0738747f345f435ad
malak akkas <akkas.malak@gmail.com>
2021-07-04 10:51:12 +0300

    malak commits

--- src/main/java/com/codingdojo/ReveaStoreProject/controllers/UserController.java
+++ src/main/java/com/codingdojo/ReveaStoreProject/controllers/UserController.java
@@ -42,7 +42,7 @@ public class UserController {
         }
         
         userService.saveWithUserRole(user);
-        return "homePage.jsp";
+        return "redirect:/login";
 
     }
     // ******************************************************************************
