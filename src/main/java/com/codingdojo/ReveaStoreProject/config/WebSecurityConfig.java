31a8abd1e8b2e30b0f59d882029ce311dfb49aee
reem <raismail10@gmail.com>
2021-07-06 07:07:04 -0700

    add images file to WebSecurityConfig

--- src/main/java/com/codingdojo/ReveaStoreProject/config/WebSecurityConfig.java
+++ src/main/java/com/codingdojo/ReveaStoreProject/config/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	    protected void configure(HttpSecurity http) throws Exception {
 	        http.
 	            authorizeRequests()
-	                .antMatchers("/css/**","/js/**", "/registration","/login","/home","/").permitAll()
+	                .antMatchers("/css/**","/js/**", "/registration","/login","/home","/","/images/**").permitAll()
 	                .antMatchers("/admin/**").hasRole("ADMIN")
 	                .anyRequest().authenticated()
 	                .and()
